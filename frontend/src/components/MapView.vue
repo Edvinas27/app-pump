c2d0f494014ba552d729719fcf8e4157ad349426
andriusandriusandrius <and.zurba@gmail.com>
2026-03-15 10:50:18 +0200

    initialzie MapView component

---
+++ frontend/src/components/MapView.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { LMap, LTileLayer, LMarker } from "@vue-leaflet/vue-leaflet"
+import { ref } from "vue"
+
+const zoom = ref(13)
+const center = ref([54.6872, 25.2797]) 
+</script>
+
+<template>
+  <l-map style="height: 500px" :zoom="zoom" :center="center">
+    <l-tile-layer
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      attribution="&copy; OpenStreetMap contributors"
+    />
+    <l-marker :lat-lng="center"></l-marker>
+  </l-map>
+</template>
