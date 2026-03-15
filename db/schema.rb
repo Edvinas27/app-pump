60fb3a757aafcf812b8e9f905fae29cd9e4ea8b8
Edvinas Montvilas <edvinas.montvilas@gmail.com>
2026-03-15 11:29:24 +0200

    run migrations

---
+++ db/schema.rb
@@ -0,0 +1,36 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[8.0].define(version: 2026_03_15_092720) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
+  create_table "cars", force: :cascade do |t|
+    t.string "brand_name", null: false
+    t.string "model", null: false
+    t.float "co2_emission", null: false
+    t.bigint "fuel_type_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["brand_name", "model", "fuel_type_id", "co2_emission"], name: "idx_cars_unique_combination", unique: true
+    t.index ["fuel_type_id"], name: "index_cars_on_fuel_type_id"
+  end
+
+  create_table "fuel_types", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_fuel_types_on_name", unique: true
+  end
+
+  add_foreign_key "cars", "fuel_types"
+end
