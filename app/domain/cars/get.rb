c0d64cecb564e6a7c7ebd5a796d8a9f9d0c499bd
Edvinas Montvilas <edvinas.montvilas@gmail.com>
2026-03-15 13:26:43 +0200

    Create domain class for creating and getting a car

---
+++ app/domain/cars/get.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+class Cars::Get
+  include Interactor::Initializer
+
+  initialize_with :params
+
+  def run
+    result = Car.includes(:fuel_type).find_by(params)
+
+    result ? format_response(result) : []
+  end
+
+  private
+
+  def format_response(car)
+    {
+      id: car.id,
+      brand_name: car.brand_name,
+      model: car.model,
+      co2_emission: car.co2_emission,
+      fuel_type: {
+        id: car.fuel_type.id,
+        name: car.fuel_type.name
+      }
+    }
+  end
+end
