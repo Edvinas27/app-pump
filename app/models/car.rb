00521514bbbb39ffde1a7e2d09f97098e4833a8b
Edvinas Montvilas <edvinas.montvilas@gmail.com>
2026-03-15 13:24:22 +0200

    Add validations to car model

--- app/models/car.rb
+++ app/models/car.rb
@@ -4,9 +4,10 @@ class Car < MainModel
   belongs_to :fuel_type
 
   validates :brand_name, :model, :fuel_type, :co2_emission, presence: true
+  validates :co2_emission, numericality: { greater_than_or_equal_to: 0 }
 
   validates :brand_name, uniqueness: {
     scope: [ :model, :fuel_type_id, :co2_emission ],
-    message: "A car with this brand, model, fuel type, and emission already exists."
+    message: "a car with this brand, model, fuel type, and emission already exists."
   }
 end
