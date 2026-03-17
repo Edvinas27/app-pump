eb292e82494b05250c6c352ca0702c40d9a7ccac
AsMyliuVGTU <239124280+AsMyliuVgtu@users.noreply.github.com>
2026-03-17 03:52:13 +0200

    ccs-38, irankis lengvai integruoti oficialia automobiliu db

---
+++ app/domain/cars/import_from_csv.rb
@@ -0,0 +1,18 @@
+require 'csv'
+
+class Cars::ImportFromCsv
+  def self.call(file_path)
+    CSV.foreach(file_path, headers: true) do |row|
+      Cars::Create.for(
+        brand_name: row["Brand"],
+        model: row["Model"],
+        co2_emission: row["Ewltp (g/km)"].to_f,
+        fuel_type_id: find_fuel_type(row["Fuel"])
+      )
+    end
+  end
+
+  def self.find_fuel_type(name)
+    FuelType.find_or_create_by(name: name)&.id
+  end
+end
